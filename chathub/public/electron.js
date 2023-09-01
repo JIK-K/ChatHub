567bc3417b72c19375c6f16418a745923c77a1af
JIK-K <87060676+JIK-K@users.noreply.github.com>
2023-09-01 15:35:47 +0900

    Initial project

---
+++ chathub/public/electron.js
@@ -0,0 +1,42 @@
+"use strict";
+exports.__esModule = true;
+var path = require("path");
+var electron_1 = require("electron");
+var isDev = require("electron-is-dev");
+var BASE_URL = "http://localhost:3000";
+var mainWindow;
+function createMainWindow() {
+    mainWindow = new electron_1.BrowserWindow({
+        width: 500,
+        height: 400,
+        webPreferences: {
+            contextIsolation: true,
+            nodeIntegration: true
+        }
+    });
+    mainWindow.once("ready-to-show", function () {
+        mainWindow.show();
+    });
+    if (isDev) {
+        mainWindow.loadURL(BASE_URL);
+        mainWindow.webContents.openDevTools();
+    }
+    else {
+        mainWindow.loadFile(path.join(__dirname, "../build/index.html"));
+    }
+    mainWindow.on("closed", function () {
+        mainWindow = null;
+    });
+    //   mainWindow.setOpacity(0.3);
+}
+electron_1.app.on("ready", function () {
+    createMainWindow();
+});
+electron_1.app.on("window-all-closed", function () {
+    electron_1.app.quit();
+});
+electron_1.app.on("activate", function () {
+    if (mainWindow === null) {
+        createMainWindow();
+    }
+});
